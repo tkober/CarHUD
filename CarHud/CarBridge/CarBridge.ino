4e64fa851e8fae6d5d95ad3acd051470d72d7f6b
Thorsten Kober <kober@semvox.de>
2016-09-05 14:10:55 +0200

    BLE importiert.

--- CarHud/CarBridge/CarBridge.ino
+++ CarHud/CarBridge/CarBridge.ino
@@ -1,6 +1,21 @@
-#include <stdlib.h>
+#include <BLEAttribute.h>
+#include <BLECentral.h>
+#include <BLECharacteristic.h>
+#include <BLECommon.h>
+#include <BLEDescriptor.h>
+#include <BLEPeripheral.h>
+#include <BLEService.h>
+#include <BLETypedCharacteristic.h>
+#include <BLETypedCharacteristics.h>
+#include <BLEUuid.h>
+#include <CurieBLE.h>
 
 #include "OBD2_PIDs.h"
+
+BLEPeripheral blePeripheral;       
+BLEService heartRateServic = BLEService("180D"); 
+
+BLECharacteristic heartRateChar = BLECharacteristic("2A37", BLENotify, 2);
 
 void setup() {
   // put your setup code here, to run once:
