8a38e6bc020b0fe27a125bebffad429d4e85e817
Thorsten Kober <kober@semvox.de>
2016-09-05 15:36:11 +0200

    ControlKnob BLE umgesetzt.

--- CarHud/CarBridge/CarBridge.ino
+++ CarHud/CarBridge/CarBridge.ino
@@ -11,18 +11,75 @@
 #include <CurieBLE.h>
 
 #include "OBD2_PIDs.h"
+#include "Commands.h"
+#include "Pins.h"
+#include "Button.h"
+#include "RotaryEncoder.h"
+#include "BLE.h"
 
 BLEPeripheral blePeripheral;       
-BLEService heartRateServic = BLEService("180D"); 
+BLEService carBridgeService = BLEService(CAR_BRIDGE_SERVICE_UUID); 
+BLECharacteristic obd2Characteristic = BLECharacteristic(CAR_BRIDGE_OBD2_CHARACTERISTIC_UUID, BLERead | BLENotify, 20);
+BLECharacteristic commandsCharacteristic = BLECharacteristic(CAR_BRIDGE_COMMANDS_CHARACTERISTIC_UUID, BLERead | BLENotify, 20);
 
-BLECharacteristic heartRateChar = BLECharacteristic("2A37", BLENotify, 2);
+PushableRotaryEncoder controlKnob = PushableRotaryEncoder(CONTROL_KNOB_ENCODER_PIN_A, CONTROL_KNOB_ENCODER_PIN_B, CONTROL_KNOB_BUTTON_PIN, 1);
+
+void knob_left(int boost);
+void knob_right(int boost);
+void knob_press();
+
+void send_command(const unsigned char command);
+
 
 void setup() {
-  // put your setup code here, to run once:
- 
+  // Setting up control knob
+  controlKnob.setOnRotateClockwise(knob_right);
+  controlKnob.setOnRotateCounterClockwise(knob_left);
+  controlKnob.setOnClick(knob_press);
+
+  // Setup BLE Peripheral
+  blePeripheral.setLocalName("CarBridge");
+  blePeripheral.setAdvertisedServiceUuid(carBridgeService.uuid());  
+  blePeripheral.addAttribute(obd2Characteristic);   
+  blePeripheral.addAttribute(commandsCharacteristic);
+  blePeripheral.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
- 
+  BLECentral central = blePeripheral.central();
+  if (central) {
+    digitalWrite(ON_BOARD_LED, HIGH);
+    
+    while(central.connected()) {
+      controlKnob.tick();
+      // TODO: OBD2 values
+    }
+
+    digitalWrite(ON_BOARD_LED, LOW);
+  }
 }
+
+
+// Private
+void knob_left(int boost) {
+  send_command(LEFT);
+}
+
+void knob_right(int boost) {
+  send_command(RIGHT);
+}
+
+void knob_press() {
+  send_command(PRESS);
+}
+
+void send_command(const unsigned char command) {
+  const unsigned char value[] = { command };
+  commandsCharacteristic.setValue(value, 1);
+}
+
+
+
+
+
+
---
+++ CarHud/CarBridge/Commands.h
@@ -0,0 +1,12 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#include <stdlib.h>
+
+#define LEFT        0x01
+#define RIGHT       0x02
+#define PRESS       0x03
+#define LONG_PRESS  0x04
+
+
+#endif
