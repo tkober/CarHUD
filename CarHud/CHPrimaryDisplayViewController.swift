037043e419423043db093c373152734eb6261026
Thorsten Kober <kober@semvox.de>
2016-09-07 10:39:39 +0200

    Refactoring

---
+++ CarHud/CHPrimaryDisplayViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CHPrimaryDisplayViewController.swift
+//  CarHud
+//
+//  Created by Thorsten Kober on 07/09/16.
+//  Copyright © 2016 Thorsten Kober. All rights reserved.
+//
+
+import UIKit
+
+class CHPrimaryDisplayViewController: UIViewController {
+
+}
